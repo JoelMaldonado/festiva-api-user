e8844b7b057ff189b872696c548fdb58cbe0333b
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2025-09-30 18:49:21 -0500

    Agregar soporte HTTPS en el adaptador Fastify y configurar opciones de producción

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,30 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
-import { single } from 'rxjs';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const logger = new Logger('Festiva');
+
+  const CERT_PATH = '/etc/letsencrypt/live/api.festiva.no/fullchain.pem';
+  const KEY_PATH = '/etc/letsencrypt/live/api.festiva.no/privkey.pem';
+
+  const httpsOptions = {
+    key: fs.readFileSync(KEY_PATH),
+    cert: fs.readFileSync(CERT_PATH),
+  };
+
+  const isProduction = process.env.PRODUCTION === 'true';
+
+  const adapter = new FastifyAdapter({
+    trustProxy: true,
+    logger: false,
+    https: isProduction ? httpsOptions : undefined,
+  });
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({
-      trustProxy: true,
-      logger: false,
-    }),
+    adapter,
   );
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.setGlobalPrefix('api');
